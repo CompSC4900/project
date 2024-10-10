926262b6a82435bfc882a5b9ac4f12ca59dd9eec
Canon-R <fzm581@mocs.utc.edu>
2024-10-10 19:27:23 -0400

    Finished User Input Validation
    Removed username field.
    Added input and validation for first/last name, street, city, state, zip, gender, phone.
    Added some CSS to make the form more "appealing"
    CSS was getting long for webpages and are now in their own files.
    Edited code to compensate for regExp() addition

--- web/accounts.js
+++ web/accounts.js
@@ -32,70 +32,165 @@ function login() {
 }
 
 /**
- * Extract the new account's username, email, and password from the elements
- * with corresponding ids. Make sure that the email and username are unique,
- * and that the password matches the text in the element with id "password2".
- * If all this succeeds, log the user in, otherwise, display an error message.
+ * Extract the new account's name (first and last), address (street, city, state, zip), date of birth, gender
+ * phone number, email, and password from form using input tag IDs. The first external function call is to regExp(), which 
+ * validates user input. The second, addUser(), adds the user to session storage if input passes all validation tests
+ * Once the user is successfully added, the user will be redirected to the login page to sign in.
  * TODO: (and duplicate server-side code)
  * TODO: email verification
  **/
 function createAccount() {
 	// stuff we need to pull from the HTML
-	let requirements = ["username", "email", "password", "password2"];
+	let requirements = ["fname","lname","address","city", "state", "zip","DoB", "phoneNum", "email", "password", "password2"];
 	// "user" object with "requirements" as keys
 	let user = {};
 	// get the requirements
 	requirements.forEach(requirement => {
-		requirementHTML = document.getElementById(requirement);
-		user[requirement] = requirementHTML.value;
+			let requirementHTML = document.getElementById(requirement);
+			user[requirement] = requirementHTML.value;
 	});
-	// Regular Expressions
-	// Email: requires a series of letters and numbers before the @ symbol, the @ symbol, a series of letters and numbers for the domain name after @, a "." before the top-level domain, a top-level domain at least 2 characters long that can only be letters  
-	const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-	// Password: requires at least 1 upper and lower case letter, a number, a special character [!, @, $], and is at least 8 characters in length 
-	const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@$])[A-Za-z\d!@$]{8,}$/;
-	/** The choice of format below for user validation is so that multiple error messages 
-	 * can be displayed at a time. Also, messages can update properly when a user updates a field and resubmits form
-	 */
-	if(!emailRegex.test(user.email)&&!passwordRegex.test(user.password)){// if both are invalid, we want to display both error messages at one time
-		displayMessage("Email format is invalid.", "illegalEmail");
-		displayMessage("Weak Password! It must be at least 8 characters long and contain 1 upper and lower case letter, a number, and a special character (!, @, $)", "illegalPassword");
-		return;
-	}else if(!emailRegex.test(user.email)){ // checks if only email is invalid
-		document.getElementById("illegalPassword").hidden = true; // error message for user password should be hidden since it's valid
-		displayMessage("Email format is invalid.", "illegalEmail");
-		return;
-	}else if(!passwordRegex.test(user.password)){ // checks if only password is invalid
-		document.getElementById("illegalEmail").hidden = true; // error message for user email should be hidden since it's valid
-		displayMessage("Weak Password! It must be at least 8 characters long and contain 1 upper and lower case letter, a number, and a special character (!, @, $)", "illegalPassword");
-		return;
-	}else{ // both are valid. Hide all error messages
-		document.getElementById("illegalEmail").hidden = true;
-		document.getElementById("illegalPassword").hidden = true;
-	}
-
-	// make sure passwords match
-	if (user.password != user.password2) {
-		displayMessage("Passwords do not match", "mismatchedPassword");
-		return;
-	}
-	delete user.password2;
-	// now that we've validated everything, we can attempt to add the user to the database
-	if (!addUser(user)) {
-		return; //username or email is a duplicate
-	}
+	//Gender is handled differently
+	let gender = document.querySelector('input[name="gender"]:checked');
+	user['gender'] = gender ? gender.value : null; //Get the value of selected gender radio button, or null if not selected
 	
-	// Account Creation Success: redirect the user to the login page
-	window.location.href = "login.html";
+	if(!regExp(user)){
+		return; //input failed tests
+	}else{ //input passed all tests
+		delete user.password2;
+		//add user to session storage
+		addUser(user);
+		// Account Creation Success: redirect the user to the login page
+		window.location.href = "login.html";
+	}
 }
 
 /**
- * Add a user to the database if there is not yet a user with the given
- * username or email. Displays an error message on failure, and returns whether
- * the user was successfully added.
+ * This is the user validation function. All fields in the form
+ * have some type of format requirement, and some may have more than one type of validation test performed. 
+ */
+function regExp(user){
+	let valid = true;
+
+	//First Name, Last Name, and Address: Cannot exceed 50 characters
+	const characterLimit50 = /^.{0,50}$/;
+	if(!characterLimit50.test(user.fname)){
+		displayMessage(null, "illegalFName");
+		valid = false;
+	}else{
+		document.getElementById("illegalFName").style.display = 'none';
+	}
+	if(!characterLimit50.test(user.lname)){
+		displayMessage(null, "illegalLName");
+		valid = false;
+	}else{
+		document.getElementById("illegalLName").style.display = 'none';
+	}
+	if(!characterLimit50.test(user.address)){
+		displayMessage(null, "illegalAddress");
+		valid = false;
+	}else{
+		document.getElementById("illegalAddress").style.display = 'none';
+	}
+	//City: Cannot exceed 20 characters
+	const characterLimit20 = /^.{0,20}$/;
+	if(!characterLimit20.test(user.city)){
+		displayMessage(null, "illegalCity");
+		valid = false;
+	}else{
+		document.getElementById("illegalCity").style.display = 'none';
+	}
+	//State: must select a state
+	if (user.state === "") {
+        displayMessage(null, "illegalState");
+        valid = false;
+    } else {
+        document.getElementById("illegalState").style.display = 'none';
+    }
+	//Zip: a 5-digit number
+	const zipRegex = /^\d{5}$/;
+	if(!zipRegex.test(user.zip)){
+		displayMessage(null, "illegalZip");
+		valid = false;
+	}else{
+		document.getElementById("illegalZip").style.display = 'none';
+	}
+	//Date of Birth: between the year 1900 and current year (no min age requirement right now)
+	const dob = new Date(user.DoB);
+	const year = dob.getFullYear();
+	const currentYear = new Date().getFullYear();
+	if(year < 1900 || year > currentYear){
+		displayMessage(null, "illegalDoB");
+		valid = false;
+	}else{
+		document.getElementById("illegalDoB").style.display = 'none';
+	}
+	//Gender: must select one of the options
+	if(user.gender == null){
+		displayMessage(null, "illegalGender");
+		valid = false;
+	}else{
+		document.getElementById("illegalGender").style.display = 'none';
+	}
+	//Phone Number: must be in (xxx) xxx-xxxx
+	const phoneNumRegex = /^\(\d{3}\) \d{3}-\d{4}$/;
+	if(!phoneNumRegex.test(user.phoneNum)){
+		displayMessage(null, "illegalPhoneNum");
+		valid = false;
+	}else{
+		document.getElementById("illegalPhoneNum").style.display = 'none';
+	}
+	// Email: requires a series of letters and/or numbers before the @ symbol, the @ symbol, a series of letters and/or numbers for the domain name after @, a "." before the top-level domain, a top-level domain at least 2 letters long (no numbers/special characters)
+	const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+	if(!emailRegex.test(user.email)){
+		displayMessage("*Invalid email", "illegalEmail");
+		valid = false;
+	}else{
+		// retrieve the users list from local storage and parse it into a js array
+		let currentUsers = window.sessionStorage.getItem("users");
+		if (currentUsers == null) {
+			currentUsers = [];
+		} else {
+			currentUsers = JSON.parse(currentUsers);
+		}
+		//checks if there is any duplicate emails
+		let found = false;
+		for(otherUser of currentUsers){
+			if(otherUser.email.toLowerCase() == user.email.toLowerCase()){
+				displayMessage("*This email is already being used", "illegalEmail");
+				valid = false;    
+				found = true;
+				break;
+			}
+		}
+		if(!found){
+			document.getElementById("illegalEmail").style.display = 'none';
+		}
+	}
+	// Password: requires at least 1 upper and lower case letter, a number, a special character [!, @, $], and is at least 8 characters in length 
+	const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@$])[A-Za-z\d!@$]{8,}$/;
+	if(!passwordRegex.test(user.password)){
+		displayMessage(null, "illegalPassword");
+		valid = false;
+	}else{
+		document.getElementById("illegalPassword").style.display = 'none';
+	}
+	//checks if the retyped password matches
+	if(user.password != user.password2){
+		displayMessage(null, "mismatchedPassword");
+		valid = false;
+	}else{
+		document.getElementById("mismatchedPassword").style.display = 'none';
+	}
+
+	return valid;
+}
+
+/**
+ * Add a user to the database
  * TODO: change from local to server-side storage
  **/
 function addUser(newUser) {
+	
 	// retrieve the users list from local storage and parse it into a js array
 	let users = window.sessionStorage.getItem("users");
 	if (users == null) {
@@ -103,29 +198,12 @@ function addUser(newUser) {
 	} else {
 		users = JSON.parse(users);
 	}
-	// validate that the new user's username (case sensitive) and email is unique (case insensitive)
-	for (otherUser of users) { //Same logic applies here as in the reg exp tests. Want the error messages to update with user changes and resubmissions
-		if(otherUser.username == newUser.username && otherUser.email.toLowerCase() == newUser.email.toLowerCase()){
-			displayMessage("That username already exists", "illegalUsername");
-			displayMessage("That email is already being used", "illegalEmail");
-			return false;
-		}
-		if (otherUser.username == newUser.username) {
-			document.getElementById("illegalEmail").hidden = true;
-			displayMessage("That username already exists", "illegalUsername");
-			return false;
-		} else if (otherUser.email.toLowerCase() == newUser.email.toLowerCase()) {
-			document.getElementById("illegalUsername").hidden = true;
-			displayMessage("That email is already being used", "illegalEmail");
-			return false;
-		}
-	}
+	
 	// add the new user and transform the users array back into JSON
 	users.push(newUser);
 	users = JSON.stringify(users);
 	// update the session storage
 	window.sessionStorage.setItem("users", users);
-	return true;
 }
 
 /**
@@ -133,7 +211,8 @@ function addUser(newUser) {
  **/
 function displayMessage(message, messageId) {
 	let messageHTML = document.getElementById(messageId);
-	messageHTML.textContent = message;
-	messageHTML.append(document.createElement("br"));
-	messageHTML.hidden = false;
+	if(message != null){
+		messageHTML.textContent = message;
+	}
+	messageHTML.style.display = 'inline';
 }
