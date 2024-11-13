d1302ba4f02f0cdbbc42c7b365a4494abf928e18
Andrew Hicks <human11111010100@gmail.com>
2024-11-13 11:33:41 -0500

    fixed account creation form so it uses a ModelForm

--- clinic360/accounts/views.py
+++ clinic360/accounts/views.py
@@ -3,8 +3,7 @@ from django.template import loader
 from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
 from .models import Clinic360User
-from .forms import CreateAccountForm
-from datetime import datetime
+from .forms import Clinic360UserCreationForm
 
 def index(request):
     return redirect('/login/')
@@ -15,23 +14,15 @@ def login(request):
 
 def createAccount(request):
     if request.method == "POST":
-        form = CreateAccountForm(request.POST)
-        if form.is_valid():
-            Clinic360User.objects.create_user(
-                form.cleaned_data["email"],
-                form.cleaned_data["first_name"],
-                form.cleaned_data["last_name"],
-                form.cleaned_data["address"],
-                form.cleaned_data["city"],
-                form.cleaned_data["state"],
-                form.cleaned_data["zip_code"],
-                form.cleaned_data["birth_date"],
-                form.cleaned_data["gender"],
-                form.cleaned_data["phone_number"],
-                form.cleaned_data["password"],
-            )
+        form = Clinic360UserCreationForm(request.POST)
+        try:
+            print("success")
+            form.save()
             return redirect("/login/")
+        except ValueError:
+            print("error")
+            pass
     else:
-        form = CreateAccountForm()
+        form = Clinic360UserCreationForm()
         
     return render(request, "create-account.html", context={"form": form})
