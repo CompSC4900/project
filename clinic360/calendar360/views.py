9524f2dcf6a07300fef4d71f2f5a5f8665385252
Zephirin Schmidt <zephirin@gmail.com>
2024-11-15 13:13:00 -0500

    Set up calendar360 views file for appointment form processing, changed the project url path for the calendar for consistency

--- clinic360/calendar360/views.py
+++ clinic360/calendar360/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .forms import CreateAppointmentForm
 
@@ -9,8 +9,12 @@ def index(request):
     return HttpResponse(template.render())
 
 def calendar(request):
-    template = loader.get_template("calendar.html")
-    # return HttpResponse(template.render())
+    if request.method == 'POST':
+        form = CreateAppointmentForm(request.POST)
+        if form.is_valid():
+            return HttpResponse('Yay!')
+    else:
+        form = CreateAppointmentForm()
 
-    form = CreateAppointmentForm()
+    
     return render(request, "calendar.html", context={"form": form})
