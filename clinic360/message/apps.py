bf9a29e70a031e62720a74ba51006e8a146b0c63
LordTanooki <jgz273@mocs.utc.edu>
2025-02-21 15:23:31 -0500

    Update apps.py

--- clinic360/message/apps.py
+++ clinic360/message/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+#Pulled from the Django app configuration class.
 
 
 class MessageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'message'
+#Defines a class with a string named message. This allows for the app to be controlled based on the string.
