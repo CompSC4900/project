55d7e6724d089d3525a03a5c627521cd98c6958a
Andrew Hicks <rdr398@mocs.utc.edu>
2025-02-22 10:52:55 -0500

    Revert "Update vite.config.ts"
    This reverts commit 8cfecb301c83efe5ddc7d2df66daa4c03ebf4308.

--- clinic360-fe/vite.config.ts
+++ clinic360-fe/vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-#This shows the plug-in being imported from Vite.
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
-#The plug-in is instructed to react with the default tool.
