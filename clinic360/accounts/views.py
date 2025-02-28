1ce2a16aca0e6adce5b9bb8d2dc6ae4f42ae4732
LordTanooki <jgz273@mocs.utc.edu>
2025-02-28 10:00:17 -0500

    Update views.py

--- clinic360/accounts/views.py
+++ clinic360/accounts/views.py
@@ -5,6 +5,7 @@ from .serializers import CreateAccountSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
+#Import models and classes from rest_framework and Clinic360.
 
 class CreateAccountView(generics.CreateAPIView):
     queryset = Clinic360User.objects.all()
@@ -13,11 +14,13 @@ class CreateAccountView(generics.CreateAPIView):
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
+#Restrictions placed on those who are authenticated to access information.
 def user_info(request):
     full_name = request.user.first_name + " " + request.user.last_name
     return Response({
         "name": full_name
     })
+    #Defined request for full name in user info.
 
 # API endpoint to return if user is staff or not
 @api_view(['GET'])
