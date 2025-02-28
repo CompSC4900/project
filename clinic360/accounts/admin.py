23532f16e6caa9b49e56469e4aa375fef161a5da
LordTanooki <jgz273@mocs.utc.edu>
2025-02-27 22:36:42 -0500

    Update admin.py

--- clinic360/accounts/admin.py
+++ clinic360/accounts/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from .models import Clinic360User
 
 admin.site.register(Clinic360User)
+#These lines of code serve to facilitate user registration within the Django project.
