d3c7aa04be56138a773364f6969bd9ec0cc36ab7
LordTanooki <jgz273@mocs.utc.edu>
2025-02-27 22:55:18 -0500

    Update apps.py

--- clinic360/accounts/apps.py
+++ clinic360/accounts/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+#Import AppConfig class from the Django application
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+#Class for defining the accounts and how 'name' is defined by the account info.
