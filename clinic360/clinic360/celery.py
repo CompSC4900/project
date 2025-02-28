cff28a205c9998ca446b5c6824de820362cd2acc
LordTanooki <jgz273@mocs.utc.edu>
2025-02-28 10:11:56 -0500

    Update celery.py

--- clinic360/clinic360/celery.py
+++ clinic360/clinic360/celery.py
@@ -1,6 +1,7 @@
 from celery import Celery
 from celery.schedules import crontab
 import os
+#Establish interaction with the operating system and imports celery to run tasks periodically.
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clinic360.settings')
 
@@ -8,10 +9,12 @@ app = Celery('clinic360')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 app.conf.broker = 'django://'
+#Discover task configuration automatically within the celery class.
 
 app.conf.beat_schedule = {
     'create_appointment_days': {
         'task': 'appointment.tasks.create_appointment_days',
         'schedule': crontab(minute=0, hour='*'),
     },
+    #Configure the time the tasks create an appointment by the minute and hour.
 }
