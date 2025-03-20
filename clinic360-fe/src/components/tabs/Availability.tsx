73b7a107994e1a51b319be811408bdf1103f2099
Zephirin Schmidt <zephirin@gmail.com>
2025-03-19 22:55:12 -0400

    added availability validation to the frontend (making sure unblocked time slots are continuous)

--- clinic360-fe/src/components/tabs/Availability.tsx
+++ clinic360-fe/src/components/tabs/Availability.tsx
@@ -55,7 +55,6 @@ export default function Scheduling() {
 
   useEffect(() => {
     localStorage.setItem("blockedWeeks", JSON.stringify(blocked));
-    saveBlocked(); // Send data to backend when availability grid changes, if load on the server ever becomes a problem we'll want to make a dedicated save button to do this instead.
   }, [blocked]);
 
   const toggleBlock = (dayIndex, timeIndex) => {
@@ -139,9 +138,48 @@ export default function Scheduling() {
       }
   
       console.log("Blocked schedule saved successfully");
+      alert("Schedule saved!");
     } catch (error) {
       console.error("Error saving blocked schedule:", error);
+      alert("Failed to save schedule. Please try again.");
     }
+  };
+
+  // function to determine if unblocked time slots in availability grid are continuous for each day or not
+  function isContinuous(blocked: Record<string, boolean[][]>): boolean {
+    for (const [_, days] of Object.entries(blocked)) { // iterate over each week in 'blocked'
+      for (const day of days) { // iterate over each day of the current week
+        let falseSeriesCount = 0;
+
+        if (day[0] === false) {
+          falseSeriesCount = 1;
+        }
+
+        for (let i = 1; i < day.length; i++) { // use index to iterate over each time slot of the day
+          if (day[i] === false && day[i-1] === true) {
+            falseSeriesCount += 1;
+          }
+        }
+        if (falseSeriesCount > 1) {
+          return false;
+        }
+      }
+    }
+  
+    return true;
+  }
+  
+  // function to create alerts verifying if provided availability is acceptable or not
+  function validateBlocked(blocked: Record<string, boolean[][]>): void {
+    if (isContinuous(blocked) === false) {
+      alert("BAD")
+    } else {
+      alert("GOOD")
+    }
+  }
+
+  const handleCheckAvailability = () => {
+    validateBlocked(blocked)
   };
 
   return (
@@ -209,6 +247,9 @@ export default function Scheduling() {
               );
             })}
           </div>
+          <button onClick={handleCheckAvailability} style={{ marginTop: 20, padding: 10, background: "blue", color: "white" }}>
+            Save Schedule
+          </button>
         </div>
 
         {/* DayView for the selected day */}
