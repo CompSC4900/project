b277365a3ffff3a0a116b9d0af0ad05c6bee991b
LordTanooki <jgz273@mocs.utc.edu>
2025-03-23 13:27:43 -0400

    Update Appointment.ts

--- clinic360-fe/src/util/Appointment.ts
+++ clinic360-fe/src/util/Appointment.ts
@@ -1,6 +1,8 @@
 import { expectSuccess } from "./auth";
 import { AuthFunctions } from "../components/AuthContext";
+//API and authorization components for appointment functions.
 
+//Interface for the appointment preview that shows the date, number, doctor, patient, duration, and ID number.
 export interface AppointmentPreview {
     id: number;
     name: string;
@@ -10,12 +12,14 @@ export interface AppointmentPreview {
     patient: string;
 }
 
+//Extension of interface appointment showing the description, status, and appointment type.
 export interface Appointment extends AppointmentPreview {
     description: string;
     status: string;
     appointment_type: string;
 }
 
+//Interface with boolean for the upcoming patient and duration number.
 export interface AppointmentType {
     id: number;
     name: string;
@@ -23,6 +27,7 @@ export interface AppointmentType {
     patient_facing: boolean;
 }
 
+//Interface for the appointment day with available slots placed into a list along with the appointments.
 export interface AppointmentDay {
     id: number;
     available_slots: Date[];
@@ -36,6 +41,7 @@ export interface AppointmentSettings {
     appointment_slot_duration: number;
 }
 
+//Interface for the Appointment Creation information with the day, time, appointment type, and doctor number.
 export interface AppointmentCreationInfo {
     dayId: number;
     time: Date;
@@ -43,12 +49,14 @@ export interface AppointmentCreationInfo {
     doctorId: number;
 }
 
+//Interface for the provider's full name.
 export interface Provider {
     id: number;
     first_name: string;
     last_name: string;
 }
 
+//Function to fetch the protected data about the appointment date.
 export async function getAppointmentDays(auth: AuthFunctions, month: number, year: number): Promise<AppointmentDay[]> {
     const response = await auth.fetchProtectedData(`appointment/days/?month=${month}&year=${year}`, "GET");
     expectSuccess(response, auth);
@@ -91,6 +99,7 @@ export async function scheduleAppointment(auth: AuthFunctions, appointment: Appo
     expectSuccess(response, auth);
 }
 
+//Function for rescheduling an appointment based on patient and appointment info.
 export async function rescheduleAppointment(auth: AuthFunctions, oldAppointment: AppointmentPreview, newAppointment: AppointmentCreationInfo): Promise<void> {
     const response = await auth.fetchProtectedData(`appointment/patient/reschedule/`, "POST", {
         id: oldAppointment.id,
@@ -99,6 +108,7 @@ export async function rescheduleAppointment(auth: AuthFunctions, oldAppointment:
     expectSuccess(response, auth);
 }
 
+//Function for canceling an appointment based on patient and appointment info.
 export async function cancelAppointment(auth: AuthFunctions, appointment: AppointmentPreview): Promise<void> {
     const response = await auth.fetchProtectedData(`appointment/patient/cancel/${appointment.id}/`, "DELETE");
     expectSuccess(response, auth);
