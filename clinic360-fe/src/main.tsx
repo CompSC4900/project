bfd03d6600d7ffc46568084d936e1478a688b79f
LordTanooki <jgz273@mocs.utc.edu>
2025-03-23 20:36:14 -0400

    Update main.tsx

--- clinic360-fe/src/main.tsx
+++ clinic360-fe/src/main.tsx
@@ -5,9 +5,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App.tsx'
+//Importing modules and bootstrap components for root creation.
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
     </StrictMode>,
 )
+//Renders the new root API and places it under Strict Mode.
