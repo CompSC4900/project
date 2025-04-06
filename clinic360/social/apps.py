356160fa4bf192bc0a65776b31e232687ffc2de7
LordTanooki <jgz273@mocs.utc.edu>
2025-04-06 12:25:55 -0400

    Update apps.py

--- clinic360/social/apps.py
+++ clinic360/social/apps.py
@@ -1,6 +1,9 @@
+#Import AppConfig from list of Django applications.
 from django.apps import AppConfig
 
-
+#Configuration class from AppConfig.
 class SocialConfig(AppConfig):
+    #All apps will use BigAutoField as default type.
     default_auto_field = 'django.db.models.BigAutoField'
+    #Name used by Django to define location.
     name = 'social'
