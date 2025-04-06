00cfdc0bfddc8172a3aad6ec1bd18f253106dbad
LordTanooki <jgz273@mocs.utc.edu>
2025-04-06 12:23:38 -0400

    Update admin.py

--- clinic360/social/admin.py
+++ clinic360/social/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+#Import the specified models.
 from .models import SocialInfo, Condition, FriendRequest
 
+#Register the specified models for admin.
 admin.site.register(SocialInfo)
 admin.site.register(Condition)
 admin.site.register(FriendRequest)
