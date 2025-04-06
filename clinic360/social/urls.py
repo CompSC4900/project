af3961da5d04f0dc3180bcde45ffc6e981e3f5f1
LordTanooki <jgz273@mocs.utc.edu>
2025-04-06 13:48:18 -0400

    Update urls.py

--- clinic360/social/urls.py
+++ clinic360/social/urls.py
@@ -1,12 +1,17 @@
+#Import Django modules.
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConditionViewSet, PatientSocialInfoView, StaffSocialInfoViewSet, IncomingFriendRequestView, OutgoingFriendRequestView, accept_friend_request, get_friends, SelfSocialInfoView, remove_friend
 
+#Router intialization.
 router = DefaultRouter()
+#Registers the conditions and staff endpoints.
 router.register('conditions', ConditionViewSet, basename='conditions')
 router.register('staff', StaffSocialInfoViewSet, basename='staff-social-info')
 
+#Make an array for URL patterns.
 urlpatterns = [
+    #Most of these paths are for friend requests and the URLs for engaging with them.
     path('', include(router.urls)),
     path('friends/incoming/', IncomingFriendRequestView.as_view(), name='incoming-friend-requests'),
     path('friends/incoming/<int:pk>/', IncomingFriendRequestView.as_view(), name='incoming-friend-request-detail'),
