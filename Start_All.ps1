9a65a91affb0119e53df22bc1b2f1c1c1ae4af64
Kadn0 <kadensmith8@gmail.com>
2025-04-11 15:57:30 -0400

    Start file

--- Start_All.ps1
+++ Start_All.ps1
@@ -1,4 +1,34 @@
-venv\Scripts\activate
+# Update pip first (optional, but often helpful)
+python -m pip install --upgrade pip==24.3.1
+
+# Install the listed packages with specified versions
+python -m pip install amqp
+python -m pip install asgiref
+python -m pip install billiard
+python -m pip install celery
+python -m pip install click
+python -m pip install click-didyoumean
+python -m pip install click-plugins
+python -m pip install click-repl
+python -m pip install Django
+python -m pip install django-celery-results
+python -m pip install django-cors-headers
+python -m pip install djangorestframework
+python -m pip install djangorestframework_simplejwt
+python -m pip install kombu
+python -m pip install pillow
+python -m pip install prompt_toolkit
+python -m pip install PyJWT
+python -m pip install python-dateutil
+python -m pip install pytz
+python -m pip install six=
+python -m pip install sqlparse
+python -m pip install tzdata
+python -m pip install vine
+python -m pip install wcwidth
+
+Write-Host "All packages installed!"
+
 #Changes directory to Clinic360
 cd .\Clinic360
 
